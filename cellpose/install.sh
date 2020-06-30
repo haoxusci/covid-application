89e5d422e7a9a5b6adda424132c95e824e35a609
hao.xu <xuhaonju@gmail.com>
2020-06-30 11:47:27 +0200

    generate mask

---
+++ cellpose/install.sh
@@ -0,0 +1,15 @@
+# cellpose
+pip install cellpose==0.0.1.22
+pip uninstall mxnet-mkl
+pip uninstall mxnet
+pip install mxnet-cu101
+# dependencies
+#pip install glob
+#pip install imageio
+#pip install scikit-image
+#pip install scipy
+#pip install opencv-python
+# Imjoy
+pip install imjoy
+pip install -U imjoy-jupyter-extension
+pip install asyncio
